d1fc16d49d5ea9e17e22db47fefead19cd9747a2
arindam31 <thearc31@gmail.com>
2018-12-02 13:03:55 +0530

    Memoize function update: If function argument values are different, function recalculates value and replaces result bank with new argument and results. Test for the same also added

--- memoization.py
+++ memoization.py
@@ -4,6 +4,7 @@ import datetime as dt
 """
 Function: memoize
 I/P: function, args, timeout
+O/P: result of calculation
 """
 memoize_dict = {}
 
@@ -20,12 +21,14 @@ def timer_expired(timeout, expiry_time=None):
 
 def memoize(func, *args):
 	if func.__name__ in memoize_dict.keys():
-		return memoize_dict[func.__name__]['result'], 'Memoized'
-	else:
-		result = func(*args)
-		memoize_dict[func.__name__] = {'arg': args, 'result': result}
-		#timer_expired(timeout=5000)
-		return result, 'First Calculation'
+		if set(args) == set(memoize_dict[func.__name__]['arg']):
+			return memoize_dict[func.__name__]['result'], 'Memoized'
+		
+
+	result = func(*args)
+	memoize_dict[func.__name__] = {'arg': args, 'result': result}
+	#timer_expired(timeout=5000)
+	return result, 'First Calculation'
 
 
 
@@ -35,7 +38,8 @@ if __name__ == '__main__':
 	result = memoize(fun1, 10, 20)
 	result1 = memoize(fun1, 10, 20)
 	result2 = memoize(fun2, 10, 20)
-	print result, result1, result2
+	result3 = memoize(fun1, 99, 1)
+	print result, result1, result2, result3
 
 
 
--- test_memoization.py
+++ test_memoization.py
@@ -14,9 +14,7 @@ class MemoizeTest(unittest.TestCase):
 
 	def test_memoize_function_with_default_timeout_using_single_function_with_same_arguments(self):
 		"""
-			Test I/P: Function and arguments, no timeout
-			O/P : Return value of function
-			Steps:
+		Tests Steps:
 			1) Call memoize with function with arguments
 			2) Assert results 
 			3) Call memoize with function with same arguments
@@ -35,10 +33,10 @@ class MemoizeTest(unittest.TestCase):
 		"""
 		Test Steps: 
 		
-		1) Call function (add_func). This should be calculated by this function
-		3) Call function (add_func) with same arguments again. Result should come from memoized result bank.
-		4) Call another function (multiply_func). Result should be calculated.
-		5) Call First function withe same arguments again. Result should come from memoized result bank.
+			1) Call function (add_func). This should be calculated by this function
+			2) Call function (add_func) with same arguments again. Result should come from memoized result bank.
+			3) Call another function (multiply_func). Result should be calculated.
+			4) Call First function withe same arguments again. Result should come from memoized result bank.
 		"""
 		
 		from_memoize, message = memoization.memoize(self.add_func, 11, 22)
@@ -53,6 +51,22 @@ class MemoizeTest(unittest.TestCase):
 		self.assertEqual(from_memoize, 20)
 		self.assertEqual(message, 'First Calculation', 
 			'Expected this to be first calculation')
+
+	def test_single_function_with_default_timeout_and_different_arguments(self):
+		"""
+		Test Steps:
+			1) Call function (add_func) with values v1 and v2. This should be calculated
+			2) Call function (add_func) with values v3 and v4. This should be calculated
+			3) Call function (add_func) with values v1 and v2. This should be calculated
+		"""
+		from_memoize, message = memoization.memoize(self.add_func, 11, 22)
+		self.assertEqual(from_memoize, 33)
+		self.assertEqual(message, 'First Calculation', 
+			'Expected this to be first calculation')
+		from_memoize, message = memoization.memoize(self.add_func, 100, 200)
+		self.assertEqual(from_memoize, 300)
+		self.assertEqual(message, 'First Calculation', 'Expected this to be first calculation')
+
 
 if __name__ == '__main__':
 	unittest.main()
