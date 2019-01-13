2069c3aab085d5bf42e2d7c0aae2b04471d7301a
arindam31 <thearc31@gmail.com>
2019-01-13 12:55:23 +0530

    Final changes in print statements for timer performance demo

--- myfunctions.py
+++ myfunctions.py
@@ -45,66 +45,66 @@ memoize_fact = memoize.memoize(factorial)
 
 
 if __name__ == '__main__':
-    # Without Memoization (Factorial and Fibinacci functions)
-    # 1st SET
+    print "Below are timings achieved using various memoization techniques" \
+    " Also shown along is cache behaviour and timer expiry"
 
-    print '---------------- First calculation timing: 1st Set Direct function call ---------------------'
-    print timeit.timeit('fibonacci(7)', setup='from __main__ import fibonacci', number=1)
-    print timeit.timeit('factorial(5)', setup='from __main__ import factorial', number=1)
+    print '---------------- Direct function call (fibonacci and factorial functions). No memoization. ---------------------'
+    print "fibonacci(20)", timeit.timeit('fibonacci(20)', setup='from __main__ import fibonacci', number=1)
+    print "factorial(5)", timeit.timeit('factorial(5)', setup='from __main__ import factorial', number=1)
 
 
-    # Using memoization
-    print '---------------- First calculation timing: With Memoization 1st Set ---------------------'
-    print timeit.timeit('memoize_fib(20)', setup='from __main__ import memoize_fib', number=1)
-    print timeit.timeit('memoize_fact(5)', setup='from __main__ import memoize_fact', number=1)
+    # Using memoization function "memoize" which does not support arguments
+    print '---------------- First calculation timings with 1st set arguments: With Memoization ---------------------'
+    print "memoize_fib(20)", timeit.timeit('memoize_fib(20)', setup='from __main__ import memoize_fib', number=1)
+    print "memoize_fact(5)", timeit.timeit('memoize_fact(5)', setup='from __main__ import memoize_fact', number=1)
 
 
     # Below two statements should show almost same time as above.
     # 1st Set Repeat
-    print '----------------------1st Set Repeat timings-----------------------------'
-    print timeit.timeit('memoize_fib(20)', setup='from __main__ import memoize_fib', number=1)
-    print timeit.timeit('memoize_fact(5)', setup='from __main__ import memoize_fact', number=1)
+    print '----------------------Repeat with 1st Set arguments : With Memoization -----------------------------'
+    print "memoize_fib(20)", timeit.timeit('memoize_fib(20)', setup='from __main__ import memoize_fib', number=1)
+    print "memoize_fact(5)", timeit.timeit('memoize_fact(5)', setup='from __main__ import memoize_fact', number=1)
 
 
     # 2nd Set
-    print '-----------------First calculation 2nd Set timings-----------------------'
-    print timeit.timeit('memoize_fib(5)', setup='from __main__ import memoize_fib', number=1)
-    print timeit.timeit('memoize_fact(8)', setup='from __main__ import memoize_fact', number=1)
+    print '-----------------First calculation timings with 2nd Set: With Memoization -----------------------'
+    print "memoize_fib(5)", timeit.timeit('memoize_fib(5)', setup='from __main__ import memoize_fib', number=1)
+    print "memoize_fact(8)", timeit.timeit('memoize_fact(8)', setup='from __main__ import memoize_fact', number=1)
 
     # This set calculation must begin to show improvement
     # 1st Set repeat
-    print '-------------------Timings : Recalculating 1st set--------------------------'
-    print timeit.timeit('memoize_fib(20)', setup='from __main__ import memoize_fib', number=1)
-    print timeit.timeit('memoize_fact(5)', setup='from __main__ import memoize_fact', number=1)
+    print '-------------------Timings : Recalculating 1st set . Again. --------------------------'
+    print "memoize_fib(20)", timeit.timeit('memoize_fib(20)', setup='from __main__ import memoize_fib', number=1)
+    print "memoize_fact(5)", timeit.timeit('memoize_fact(5)', setup='from __main__ import memoize_fact', number=1)
 
     # This set calculation must begin to show improvement
     # 2nd Set repeat
     print '-------------------~Timings : Recalculating 2nd set--------------------------'
-    print timeit.timeit('memoize_fib(5)', setup='from __main__ import memoize_fib', number=1)
-    print timeit.timeit('memoize_fact(8)', setup='from __main__ import memoize_fact', number=1)
+    print "memoize_fib(20)", timeit.timeit('memoize_fib(5)', setup='from __main__ import memoize_fib', number=1)
+    print "memoize_fact(8)", timeit.timeit('memoize_fact(8)', setup='from __main__ import memoize_fact', number=1)
 
     # Let's see the cached values
-    print 'Factorial Cache'
-    print memoize_fact.__closure__[0].cell_contents
+    print '\nFactorial Cache', memoize_fact.__closure__[0].cell_contents
 
-    print 'Fibo Cache'
-    print memoize_fib.__closure__[0].cell_contents
-
-    print '-------------- Add Function memoized ------'
+    print '\nFibo Cache', memoize_fib.__closure__[0].cell_contents
+    
+    print '\n-------------- Add Function memoized: With memoization (memoize_with_args) ------'
     print "Cache for add function before calling it.:",add.__closure__[0].cell_contents
 
     print 'Memoize with multiple arguments example'
     # 1st set first call with memoize
-    print add(2,3)
-    print add(1,2,3,4,5)
+    print "Add : 2 + 3", add(2,3)
+    print "Add 1,2,3,4,5", add(1,2,3,4,5)
     print "Cache after 1st call:", add.__closure__[0].cell_contents
 
-    # 1st set second call with memoize
-    print add(2,3)
-    print add(1,2,3,4,5)
+    # 1st set,  second call with memoize
+    print "Add : 2 + 3 :", add(2,3)
+    print "Add 1,2,3,4,5 : ", add(1,2,3,4,5)
     print "Cache after 2nd call:", add.__closure__[0].cell_contents
 
-    print '-------------- Multiply Function memoized with timer check ------'
+    print '\n-------------- Multiply Function memoized with timer check : With memoization (my_memoize_with_args_and_timer) ------'
+    print " Showing cache behaviour with same arguments used multiple times"
+    
     print "Cache for multiply:", multiply.__closure__[0].cell_contents
     print multiply(2,3,5)
 
@@ -126,7 +126,7 @@ if __name__ == '__main__':
 
     
 
-    print '-------------- Testing performance of Class based Memoization technique ------'
+    print '\n -------------- Testing performance of Class based Memoization technique on "sum_of_square" function ------'
     # Note: The decorator has been set to expire cache in 2 second
     print '1st set call...Expected max time'
     print '1st call time:', timeit.timeit('sum_of_square(2,3,4)', 'from __main__ import sum_of_square', number=1)
